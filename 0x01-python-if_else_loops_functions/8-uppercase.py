24d4945cc403ff6816b96a1add69d4a375021acd
Chekwasy <richardchekwas@gmail.com>
2023-03-13 18:43:23 -0700

    aa

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-        new_str = ""
-        for i in range(len(str)):
+        ln = len(str)
+        for i in range((ln)):
                 if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
-                        new_str += chr(ord(str[i]) - 32)
-                        continue
-                new_str += str[i]
-        print('{0}'.format(new_str))
+                        val = ord(str[i]) - 32
+                else:
+                        val = ord(str[i])
+                print(chr(val), end='')
+        print("")
---
+++ 0x01-python-if_else_loops_functions/tes.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+string = "string"
+print(string[1])
