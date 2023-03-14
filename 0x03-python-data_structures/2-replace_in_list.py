8c45eee0a3e3d0d06eccf877bf9cf0b64d3f2e61
Chekwasy <richardchekwas@gmail.com>
2023-03-13 22:22:20 -0700

    agaf

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,7 +6,7 @@ def replace_in_list(my_list, idx, element):
             if idx < 0:
                 return my_list
             else:
-                mylist.insert(idx, element)
+                my_list[j] = element
                 return my_list
-            j += 1
+        j += 1
     return my_list
