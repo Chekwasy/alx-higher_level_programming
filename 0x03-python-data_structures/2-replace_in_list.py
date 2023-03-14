5cc14d763628e128c5a64c0fa9e939d17b722c64
Chekwasy <richardchekwas@gmail.com>
2023-03-13 20:23:53 -0700

    aa

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    j = 0
+    for i in my_list:
+        if j == idx:
+            if idx < 0:
+                return my_list
+            else:
+                mylist.insert(idx, element)
+                return my_list
+            j += 1
+    return my_list
