ccef6f9d9a3a86788788c1ee21de525fcf8613f1
Chekwasy <richardchekwas@gmail.com>
2023-03-28 12:37:17 -0700

    ag

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        check = False;
-        int(value)
+        check = False
         print("{:d}".format(value))
         check = True
 
