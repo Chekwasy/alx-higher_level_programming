91117831c8cfad6820edca93dd133f529d0568cd
Chekwasy <richardchekwas@gmail.com>
2023-03-28 12:08:04 -0700

    aq

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@ def safe_print_list(my_list=[], x=0):
     try:
         j = 0
         for i in my_list:
-            j += 1
-            if j <= x:
+            if j < x:
                 print("{:d}".format(i), end='')
-                print('')
-    except:
-        print("")
+                j += 1
+        print('')
+    except TypeError:
+        pass
     finally:
         return j
