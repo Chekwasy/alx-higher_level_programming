eaa278c129b2742188faae42d80db77f6d262966
Chekwasy <richardchekwas@gmail.com>
2023-03-30 04:25:15 -0700

    aahv

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""Square Class
-A Square Class
-"""
-
-
+"""Square Class"""
 class Square:
-
+"""A Square Class"""
     def __init__(self, size):
         """__init__
         The __init__ method initializes the size value
