72a1e8c90869b4edcbb648e74d50a94e136c1cac
Chekwasy <richardchekwas@gmail.com>
2023-03-30 05:21:46 -0700

    awer

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """Square Class"""
+
 class Square:
+
     """Class own Square"""
+
     def __init__(self, size):
+
         """__init__ size (int): The size of the square."""
+
         self.__size = size
