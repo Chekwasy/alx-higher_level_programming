4450b3547850127ded989f5932418f561bbdd6e2
Chekwasy <richardchekwas@gmail.com>
2023-04-04 11:02:16 -0700

    aa

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""A Program that define rectangle"""
+
+
+class Rectangle:
+    """Class Instance definition"""
+
+    def __init__(self, width=0, height=0):
+        """init initialization"""
+
+        self.width = width
+        self.height = height
+
+    """Class attribute width"""
+
+    @property
+    def width(self):
+        """Property width"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    """Another Class atrribute"""
+
+    @property
+    def height(self):
+        """height property"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    """ Area of Rectangle public insyance method"""
+
+    def area(self):
+        """Area of rectangle"""
+
+        return self.__width * self.__height
+
+    """ Perimeter public instance methd"""
+
+    def perimeter(self):
+        """It beginns"""
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return ((self.__width * 2) + (self.__height * 2))
