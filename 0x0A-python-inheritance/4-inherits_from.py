826b3da1436b116eace12804d65b3f0134e879bf
Chekwasy <richardchekwas@gmail.com>
2023-04-12 04:12:15 -0700

    qq

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,5 +8,7 @@ def inherits_from(obj, a_class):
     if isinstance(obj, a_class):
         if issubclass(a_class, obj.__class__) is False:
             return True
+        else:
+            return False
     else:
         return False
