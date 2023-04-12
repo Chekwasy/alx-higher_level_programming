815606a1f90d36ff6210356fbee3293517a23f71
Chekwasy <richardchekwas@gmail.com>
2023-04-12 02:10:54 -0700

    ga

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,4 +5,7 @@
 def is_same_class(obj, a_class):
     """Checking isistance"""
 
-    return (isinstance(obj, a_class))
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
