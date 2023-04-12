bf600d4e70f323cc06ba1cf166a02884d8e12882
Chekwasy <richardchekwas@gmail.com>
2023-04-12 04:09:56 -0700

    at

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,8 @@
 def inherits_from(obj, a_class):
     """The function to check the subclass"""
 
-    if isinstance(obj, a_class) and issubclass(a_class, obj.__class__) is False:
-        return True
+    if isinstance(obj, a_class):
+        if issubclass(a_class, obj.__class__) is False:
+            return True
     else:
         return False
