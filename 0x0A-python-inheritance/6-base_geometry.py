c67a197600959092e489bd33d37902d82c401683
Chekwasy <richardchekwas@gmail.com>
2023-04-12 04:55:10 -0700

    zz

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Empty class"""
+
+
+class BaseGeometry:
+    """raise exception"""
+
+    def area(self):
+        """raise alarm"""
+
+        raise Exception ("area() is not implemented")
