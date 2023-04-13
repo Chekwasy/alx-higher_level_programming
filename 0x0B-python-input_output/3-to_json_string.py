49690c6686153f7719ccd372888f1be95d7d9fd7
Chekwasy <richardchekwas@gmail.com>
2023-04-13 05:54:08 -0700

    agg

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import json
+from json import dumps
 """json stuff"""
 
 
 def to_json_string(my_obj):
     """The function"""
 
-    return (json.dumps(my_obj))
+    return (dumps(my_obj))
