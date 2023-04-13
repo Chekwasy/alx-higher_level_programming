d231682f145e30dbfded8462ddbeeba9a658fccb
Chekwasy <richardchekwas@gmail.com>
2023-04-13 07:01:51 -0700

    gsg

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""json stuff"""
+"""json from stuff"""
 from json import loads
 
 
 def from_json_string(my_str):
-        """The function"""
+        """The j function"""
 
         return (loads(my_str))
