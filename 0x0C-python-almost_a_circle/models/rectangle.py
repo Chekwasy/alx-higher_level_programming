50730b0064bc86787a44f19aae08f856b78e0d6c
Chekwasy <richardchekwas@gmail.com>
2023-04-19 21:09:11 -0700

    as

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -130,8 +130,8 @@ class Rectangle(Base):
 
         return {
             'id': self.id,
-            'width': self.width,
-            'height': self.height,
-            'x': self.x,
-            'y': self.y
+            'width': self.__width,
+            'height': self.__height,
+            'x': self.__x,
+            'y': self.__y
         }
