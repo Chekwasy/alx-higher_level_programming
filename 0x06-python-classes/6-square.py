5c76e6eb6657c5d92b309870945740552fe69aae
Chekwasy <richardchekwas@gmail.com>
2023-05-11 09:20:07 -0700

    aat

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -123,11 +123,13 @@ class Square:
             for i in range(self.__position[1]):
                 print('')
 
-        for j in range(1, self.area() + 1):
-            if j % self.__size == 1:
-                print('{:>{w}}'.format('#', w=self.__position[0] + 1), end='')
-            else:
+        a = 0
+        for j in range(self.__size):
+            for k in range(self.__size):
+                if k < self.__position[0] and a == 0:
+                    for l in range(self.__position[0]):
+                        print(" ", end='')
+                a = 1
                 print('#', end='')
-
-                if j % self.__size == 0 and j > 0:
-                    print()
+            print()
+            a = 0
