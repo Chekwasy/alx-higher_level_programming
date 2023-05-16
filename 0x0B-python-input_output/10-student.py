ffe302cc8292d26762fd2e03e3419867b601796a
Chekwasy <richardchekwas@gmail.com>
2023-05-16 12:07:47 -0700

    qq

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Student Class"""
+
+
+class Student:
+    """Class begin"""
+
+    def __init__(self, first_name, last_name, age):
+        """init stuff"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retriving dictionary"""
+
+        dclass = self.__dict__
+        dsel = dict()
+
+        if type(attrs) is list:
+            for atr in attrs:
+                if type(atr) is not str:
+                    return dclass
+
+                if atr in dclass:
+                    dsel[atr] = dclass[atr]
+
+                    return dsel
+
+            return dclass
+        return self.__dict__
