4deb3930107cfb32b63b0cd9bb335e373817ac3f
Chekwasy <richardchekwas@gmail.com>
2023-05-15 17:05:36 -0700

    jja

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size):
+        """instatia"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Area of square"""
+
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """str repre"""
+
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
