94d76f9477cb5db02d39bc61ccb95ca6ded291f9
Chekwasy <richardchekwas@gmail.com>
2023-05-27 18:40:46 -0700

    qq

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Rectangle Class which inherit from Base class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class begins"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialization stuff"""
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
