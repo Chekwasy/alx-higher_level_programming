385e0195203d779ff3644345547df87e3f478763
root <richardchekwas@gmail.com>
2023-06-14 16:14:05 -0700

    qy

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """fun to save all arguments"""
+import os
 import json
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-from sys import argv
-import os
 
 
 if not os.path.exists("add_item.json"):
