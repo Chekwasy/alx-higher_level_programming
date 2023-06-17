8ecfc8fa471adc69feb518276a54b4a632b38847
root <richardchekwas@gmail.com>
2023-06-17 06:19:07 -0700

    qqy

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""pascal triangle"""
+
+
+def pascal_triangle(n):
+    """function begins"""
+
+    sub = []
+    tri = []
+
+    if type(n) is not int:
+        return tri
+
+    for a in range(n):
+        new = sub[:]
+        new.append(1)
+        d = len(sub)
+        for b in range(1, d):
+            new[b] = sub[b - 1] + sub[b]
+            
+        sub = new[:]
+        tri.append(sub)
+    return tri
