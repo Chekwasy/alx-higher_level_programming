722eb00f4ab8a658710f275fab439323119c6bed
Richard <richardchekwas@gmail.com>
2023-08-15 23:18:31 +0100

    aa

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
-let stringg = '';
-if (typeof process.argv[2] === 'undefined') {stringg = 'No argument'}
-else if (typeof process.argv[3] === 'undefined') {stringg = 'Argument found'}
-else {stringg = 'Arguments found'}
+let stringg = ''
+if (typeof process.argv[2] === 'undefined') {
+  stringg = 'No argument'
+}
+else if (typeof process.argv[3] === 'undefined') {
+  stringg = 'Argument found'
+} else {
+  stringg = 'Arguments found'
+}
 console.log(stringg);
