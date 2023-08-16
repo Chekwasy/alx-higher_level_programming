bb9539cfe218c791d2423faea271e13556db7ca0
Richard <richardchekwas@gmail.com>
2023-08-17 00:49:38 +0100

    aaee

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,7 +8,7 @@ module.exports = class Rectangle {
     for (let i = 0; i < this.height; i++) {
       let str = '';
       for (let j = 0; j < this.width; j++) {
-        str = str + 'x';
+        str = str + 'X';
       }
       console.log(str);
     }
