3db3f42550b6cd0a1c64189d870d6eb1ad798e22
Richard <richardchekwas@gmail.com>
2023-08-16 01:02:58 +0100

    aa

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-function add(a, b) { return (a + b) }
+function add(a, b) { return (a + b) };
 
 const firstno = Math.floor(Number(process.argv[2]));
 const secondno = Math.floor(Number(process.argv[3]));
