ff6c71632fd9b353c02303a2f796ce905e1721b6
Richard <richardchekwas@gmail.com>
2023-08-16 22:23:24 +0100

    aaa

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,24 @@
 #!/usr/bin/node
 const len = process.argv.length;
+let tmp = 0;
 let secNum = 0;
 let bigNum = Math.floor(Number(process.argv[2]));
 if (len < 4) {
   console.log(secNum);
 } else {
   secNum = Math.floor(Number(process.argv[3]));
+  if (secNum > bigNum) {
+    secNum = bigNum;
+    bigNum = Math.floor(Number(process.argv[3]));
+  }
   for (let i = 2; i < len; i = i + 1) {
-    if (bigNum < Math.floor(Number(process.argv[i + 1]))) {
+    tmp = Math.floor(Number(process.argv[i + 1]));
+    if (bigNum < tmp) {
       secNum = bigNum;
-      bigNum = Math.floor(Number(process.argv[i + 1]));
+      bigNum = tmp;
+    }
+    if (secNum < tmp && tmp !== bigNum) {
+      secNum = tmp;
     }
   }
   console.log(secNum);
