d25a7791f605c5703425369c70b0468db8a35661
Richard <richardchekwas@gmail.com>
2023-08-17 02:19:28 +0100

    zz

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square.js') {
-    charPrint (c) {
-	if (c === undefined) {
-	    this.print();
-	} else {
-	    for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
-	}
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.width; i++) {
+      let str = '';
+      for (let j = 0; j < this.width; j++) {
+        str = str + c;
+      }
+      console.log(str);
     }
+  }
 };
