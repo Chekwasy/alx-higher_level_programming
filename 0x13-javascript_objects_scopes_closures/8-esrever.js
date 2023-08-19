d0f0c5268326cf205bc5110346357433fb9a5264
Richard <richardchekwas@gmail.com>
2023-08-19 16:36:12 +0100

    aa

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let nlist = list.reverse();
+  const nlist = [];
+  const len = list.length;
+  for (let i = len - 1; i >= 0; i--) {
+    nlist.push(list[i]);
+  }
   return nlist;
-}
+};
