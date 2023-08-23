7476a3a55c649d8c87191765c3265fb8c306573b
Richard <richardchekwas@gmail.com>
2023-08-23 09:39:45 +0100

    dd

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""script that takes in the name of a state as an argument and lists all
-cities of that state, using the database hbtn_0e_4_usa"""
-import sys
-import MySQLdb
+"""takes in the name of a state as an argument and
+lists all cities of that state,
+using the database hbtn_0e_4_usa"""
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name\
+    cur.execute("SELECT cities.name\
     FROM cities LEFT JOIN states\
     ON states.id = cities.state_id\
-    ORDER BY cities.id ASC")
+    WHERE states.name = %s\
+    ORDER BY cities.id ASC", (sys.argv[4],))
     rows = cur.fetchall()
-    for row in rows:
-        if (row[2] == sys.argv[4]):
-            print(", ".join([row[0]))
+    print(", ".join([row[0] for row in rows]))
+    cur.close()
+    db.close()
