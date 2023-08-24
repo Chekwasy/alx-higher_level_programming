31bc31b158ef6177edf1494b4fdb2a055edd201d
Richard <richardchekwas@gmail.com>
2023-08-24 12:03:05 +0100

    dd

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""lists all State objects that contain the
-letter a from the database hbtn_0e_6_usa"""
-
-
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy.schema import Table
-
+"""
+List all State objects with a
+"""
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Session = sessionmaker(engine)
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine, func
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.sql import insert, update
+
+    mysql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1],
+                                                             sys.argv[2],
+                                                             sys.argv[3])
+    engine = create_engine(mysql)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    new = State(name='Louisiana')
-    session.add(new)
+    louisiana = State(name="Louisiana")
+    session.add(louisiana)
     session.commit()
-    print(new.id)
-    session.close()
+    print(louisiana.id)
