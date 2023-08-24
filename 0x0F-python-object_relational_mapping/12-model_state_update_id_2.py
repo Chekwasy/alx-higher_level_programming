751137d25234e92e76efcc74815fa413251d47be
Richard <richardchekwas@gmail.com>
2023-08-24 21:02:46 +0100

    aa

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -2,13 +2,14 @@
 """
 List all State objects with a
 """
-if __name__ == "__main__":
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine, func
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy.sql import insert, update
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import insert, update
 
+
+if __name__ == "__main__":
     mysql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1],
                                                              sys.argv[2],
                                                              sys.argv[3])
