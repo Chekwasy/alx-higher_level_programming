fa79f1945c4f500442b294c39c69032eb2df3a71
Richard <richardchekwas@gmail.com>
2023-08-24 20:34:36 +0100

    aa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """List all State object"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-
     mysql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1],
                                                              sys.argv[2],
                                                              sys.argv[3])
